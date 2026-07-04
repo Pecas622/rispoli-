2aadbef4438b07336d424047d5a1c5ffcadc2ae4
Santino De Oto <santinodeoto@MacBook-Air-de-Santino-2.local>
2026-07-04 15:19:04 -0300

    Revert "Ajusta el botón del hero de la home a voseo (Comenzá gratis)"
    This reverts commit 2bf2906f97a77ca532c802a234dc3cf7c7ca4590.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,7 +64,7 @@ export default function Home() {
 
             <div className="hero-cta">
               <button onClick={() => setAuthModal('register')} className="btn btn-primary btn-lg">
-                Comenzá gratis <ArrowRight size={16} />
+                Comenzar gratis <ArrowRight size={16} />
               </button>
               <Link to="/cursos" className="btn btn-outline btn-lg">
                 Ver cursos
